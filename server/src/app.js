6704c8f208a3d8cf1528e0fa2b200a9cc510c2fd
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-01-19 23:28:23 +0530

    adding middleware in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
-const app = express();
 import testserverroutes from './routes/testserver.routes.js'
+import cors from 'cors'
+const app = express();
+app.use(cors());
+app.use(express.json());
 app.use("/",testserverroutes)
 export default app;
