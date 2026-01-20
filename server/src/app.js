cd1ff009d180d2ebad664cccd98c2bb450884df3
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-01-20 13:40:17 +0530

    fixed the position of errorhandler

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,6 @@ import errorhandler from './middleware/error.middleware.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(errorhandler);
 app.use("/",testserverroutes)
+app.use(errorhandler);
 export default app;
