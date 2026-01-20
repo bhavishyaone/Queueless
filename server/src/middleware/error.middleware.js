941c0068fe4858a1d5346da300a8036e396f7011
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-01-20 12:33:51 +0530

    added constants

--- server/src/middleware/error.middleware.js
+++ server/src/middleware/error.middleware.js
@@ -2,4 +2,4 @@ const errorhandler = (err,req,res,next)=>{
     console.log(err)
     return res.status(500).json({message:"Server Error"})
 }
-export default errorhandler
+export default errorhandler;
