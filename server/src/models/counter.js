59f60cc2b368b77d9e5a38e2f84dfc6c308d282c
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-01-20 13:10:41 +0530

    added Counter model

---
+++ server/src/models/counter.js
@@ -0,0 +1,17 @@
+import mongoose  from "mongoose";
+import { COUNTER_STATUS } from "../utils/constants.js";
+
+const counterschema = new mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        status:{
+            type:String,
+            enum:Object.values(COUNTER_STATUS),
+            default:COUNTER_STATUS.ACTIVE
+        }
+    },
+    {timestamps:true}
+);
+console.log(COUNTER_STATUS)
+
+export default mongoose.model("Counter",counterschema)
