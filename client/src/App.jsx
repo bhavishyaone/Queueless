086dcd272340da36af9a002c281871a8aa605e93
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-01-29 10:31:57 +0530

    client setup snd installing dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Button} from './components/ui/button.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Chaliye Shuru karte hai.</h1>
+      <Button>Kya haal hai</Button>
+      </>
+  )
+}
+
+export default App
