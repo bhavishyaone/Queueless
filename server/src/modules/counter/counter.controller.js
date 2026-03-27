52ad6317895166542e2c7f537d63f2165566fa92
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-03-27 10:15:00 +0530

    feat(counter): implement createCounter endpoint in controller

--- server/src/modules/counter/counter.controller.js
+++ server/src/modules/counter/counter.controller.js
@@ -2,3 +2,17 @@ import Counter from '../../models/counter.js';
 import { COUNTER_STATUS } from '../../utils/constants.js';
 
 // Counter controller basics
+
+export const createCounter = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const exists = await Counter.exists({ name });
+        if (exists) return res.status(400).json({ message: "Counter name already exists" });
+
+        const counter = await Counter.create({ name });
+        return res.status(201).json({ message: "Counter created successfully", counter });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "server error" });
+    }
+};
